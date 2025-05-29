75e7f4c5ae2a845990c06c28399be7febec863fe
Mateusz Kurzyna <lolc44t@gmail.com>
2025-05-29 18:21:20 +0200

    ia4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    net::TcpStream,
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -10,11 +14,25 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|f| f.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    //   println!("{:#?}", http_request);
+    println!("HTTP/1.1 200 OK\r\n\r\n");
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n");
+    stream.write_all(response.as_bytes()).unwrap();
 }
